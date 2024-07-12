3396d5fc8da16a97b78e6cd63869572dc83e5a17
Kaustuv Karki <karkikaustuv@gmail.com>
2024-07-13 00:23:40 +0545

    Working on the medditation timer

---
+++ app/meditate/[id].tsx
@@ -0,0 +1,77 @@
+import { View, Text, ImageBackground, Pressable } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import meditationImages from "@/constants/meditation-images";
+import AppGradient from "@/components/AppGradient";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
+import CustomButton from "@/components/CustomButton";
+import { TimerContext } from "@/context/TimerContext";
+
+const Meditate = () => {
+  const { id } = useLocalSearchParams();
+  const [isMeditating, setMeditating] = useState(false);
+
+  const { duration: secondsRemaining, setDuration } = useContext(TimerContext);
+  const router = useRouter();
+
+  const formattedTimeMinutes = String(
+    Math.floor(secondsRemaining / 60)
+  ).padStart(2, "0");
+  const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, "0");
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+    if (secondsRemaining === 0) {
+      setMeditating(false);
+      return;
+    }
+    if (isMeditating) {
+      timerId = setTimeout(() => {
+        setDuration(secondsRemaining - 1);
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [secondsRemaining, isMeditating]);
+
+  const toggleMeditationSessionStatus = () => {
+    setMeditating(!isMeditating);
+  };
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={meditationImages[Number(id) - 1]}
+        resizeMode="cover"
+        className="flex-1">
+        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.9)"]}>
+          <Pressable
+            onPress={() => router.back()}
+            className="absolute top-16 left-6">
+            <AntDesign name="leftcircleo" size={42} color="white" />
+          </Pressable>
+          <View className="flex-1 justify-center">
+            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
+              <Text className="text-4xl text-blue-800 font-rmono">
+                {formattedTimeMinutes}:{formattedTimeSeconds}
+              </Text>
+            </View>
+          </View>
+          <View className="mb-5">
+            <CustomButton
+              title="Adjust duration"
+              // onPress={handleAdjustDuration}
+            />
+            <CustomButton
+              title={isMeditating ? "Stop" : "Start Meditation"}
+              onPress={toggleMeditationSessionStatus}
+              containerStyles="mt-4"
+            />
+          </View>
+        </AppGradient>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default Meditate;
